3085917439dcdbea64773aa0894d322c4393405a
Everaldo <contato@albreis.com.br>
2025-06-13 13:12:58 -0300

    refatorção do router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,51 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
+import adminRoutes from './admin';
+import userRoutes from './user';
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-       component: () => import('../views/Home.vue')
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('../views/Login.vue')
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('../views/Register.vue')
-    },
-    {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: () => import('../views/Dashboard.vue')
-    },
-    {
-        path: '/bookings',
-        name: 'bookings',
-        component: () => import('../views/Bookings.vue')
-    },
-    {
-        path: '/bookings/:id',
-        name: 'booking',
-        component: () => import('../views/BookingView.vue')
-    },
-    {
-        path: '/bookings/:id/form',
-        name: 'booking-form',
-        component: () => import('../views/BookingForm.vue')
-    },
-    {
-        path: '/settings',
-        name: 'settings',
-        component: () => import('../views/Settings.vue')
-    },
-    {
-        path: '/account',
-        name: 'account',
-        component: () => import('../views/Account.vue')
-    },
+    ...userRoutes,
+    ...adminRoutes    
 ];
 
 const router = createRouter({
